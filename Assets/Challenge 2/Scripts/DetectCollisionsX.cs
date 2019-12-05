9bbc16a6e154c64a1f93d5c066034e5df6ff7cb1
rick325601 <56115492+rick325601@users.noreply.github.com>
2019-12-05 18:57:53 +0100

    Update DetectCollisionsX.cs

--- Assets/Challenge 2/Scripts/DetectCollisionsX.cs
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DetectCollisionsX : MonoBehaviour
 {
 
+    // destroys objects when they collide
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
